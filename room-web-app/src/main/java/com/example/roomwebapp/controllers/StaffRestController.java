a037f7c9fc512541d233ef3b1eb6291db9444402
Glory Odeyemi <gloryodeyemi.nike@gmail.com>
2022-11-18 17:04:20 +0100

    rest controllers

---
+++ room-web-app/src/main/java/com/example/roomwebapp/controllers/StaffRestController.java
@@ -0,0 +1,25 @@
+package com.example.roomwebapp.controllers;
+
+import com.example.roomwebapp.models.StaffMember;
+import com.example.roomwebapp.services.StaffService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/staff")
+public class StaffRestController {
+    private final StaffService staffService;
+
+
+    public StaffRestController(StaffService staffService) {
+        this.staffService = staffService;
+    }
+
+    @GetMapping
+    public List<StaffMember> getAllStaff(){
+        return staffService.getAllStaff();
+    }
+}
