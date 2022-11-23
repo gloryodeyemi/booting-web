b15ffd2e2e29566c3dc82188e36b6ae5f80be219
Glory Odeyemi <gloryodeyemi.nike@gmail.com>
2022-11-23 17:01:38 +0100

    external database & staff repository

--- room-web-app/src/main/java/com/example/roomwebapp/services/StaffService.java
+++ room-web-app/src/main/java/com/example/roomwebapp/services/StaffService.java
@@ -1,26 +1,21 @@
 package com.example.roomwebapp.services;
 
-import com.example.roomwebapp.models.Position;
+import com.example.roomwebapp.data.StaffRepository;
 import com.example.roomwebapp.models.StaffMember;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.UUID;
 
 @Service
 public class StaffService {
 
-    private static final List<StaffMember> staff = new ArrayList();
+    private final StaffRepository staffRepository;
 
-    static {
-        staff.add(new StaffMember(UUID.randomUUID().toString(), "Glory", "Odeyemi", Position.CONCIERGE));
-        staff.add(new StaffMember(UUID.randomUUID().toString(), "Pelumi", "Ajuwon", Position.FRONT_DESK));
-        staff.add(new StaffMember(UUID.randomUUID().toString(), "John", "Doe", Position.SECURITY));
-        staff.add(new StaffMember(UUID.randomUUID().toString(), "Jane", "Doe", Position.HOUSEKEEPING));
+    public StaffService(StaffRepository staffRepository) {
+        this.staffRepository = staffRepository;
     }
 
     public List<StaffMember> getAllStaff(){
-        return staff;
+        return staffRepository.findAll();
     }
 }
