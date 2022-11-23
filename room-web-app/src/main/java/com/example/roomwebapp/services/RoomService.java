07777f4db80a803a49c576cb2e6dfac80999fce5
Glory Odeyemi <gloryodeyemi.nike@gmail.com>
2022-11-23 16:17:07 +0100

    room repository

--- room-web-app/src/main/java/com/example/roomwebapp/services/RoomService.java
+++ room-web-app/src/main/java/com/example/roomwebapp/services/RoomService.java
@@ -1,21 +1,20 @@
 package com.example.roomwebapp.services;
 
+import com.example.roomwebapp.data.RoomRepository;
 import com.example.roomwebapp.models.Room;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class RoomService {
-    private static final List<Room> rooms = new ArrayList<>();
-    static {
-        for (int i=0; i<10; i++){
-            rooms.add(new Room(i, "Room " + i, "R" + i, "Q"));
-        }
+    private final RoomRepository roomRepository;
+
+    public RoomService(RoomRepository roomRepository) {
+        this.roomRepository = roomRepository;
     }
 
     public List<Room> getAllRooms(){
-        return rooms;
+        return roomRepository.findAll();
     }
 }
